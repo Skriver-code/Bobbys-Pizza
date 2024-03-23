1e8d4ac34c5dd3b829f6b83d18f8fc734c900919
Skriver <eaarlsk@students.eaaa.dk>
2024-03-23 17:37:11 +0100

    Title and menu box

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-function Navibar() 
-{
-return (
-    <nav className="navbar-toggler-icon">
-            <a>Navigationbar</a>
-            <span className="navbar-toggler-icon"></span>
-
-    </nav>
-)
+function Navibar() {
+    return (
+        <nav className="title">
+            <h1>Bobbys Pizza Bix</h1>
+            <hr className="red-line" />
+        </nav>
+    );
 }
 
-const page =(
-    <nav>
-        <h1>Bobbys bizza bix</h1>
-        <ul>
-        <li>Peppyrone</li>
-        <li>margar...</li>
-        <li>Hawaii hell</li>
-        </ul>
-    </nav>
-)
+ReactDOM.render(
+    <div>
+        <Navibar />
+        <p>{page}</p>
+    </div>,
+    document.getElementById("root")
+);
+
+
+const page = (
+    <div className="container" style={{ border: "2px solid #D2B48C", width: "50%", padding: "10px" }}>
+        <h2>Pizza</h2>
+        <nav>
+            <ul>
+                <li>Pepperoni Pizza</li>
+                <li>Margarita Pizza</li>
+                <li>Hawaii Pizza</li>
+                <li>Vesuvio Pizza</li>
+                <li>Vegetar Pizza</li>
+                <li>Kebab Pizza</li>
+            </ul>
+        </nav>
+        <h2>Kebab</h2>
+        <nav>
+            <ul>
+                <li>Kebab Kylling</li>
+                <li>Kebab Lamb</li>
+                <li>Kebab Falaffel</li>
+                
+            </ul>
+        </nav>
+    </div>
+);
+
 
 
 
 ReactDOM.render(
     <div>
         <Navibar />
-        <h1 className="header">Hello Sarah</h1>
-        <p>Did you bring the coconut?</p>
-        <ul>
-            <li>10k</li>
-            <li>13.5k</li>
-        </ul>
+        
         <p>{page}</p>
     </div>
     , document.getElementById("root"))
